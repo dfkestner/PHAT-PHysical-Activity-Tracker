f4ef33f003e7e62fd53db46339a856eeebda55c3
Tea Flores-Kestner <dfkestner@gmail.com>
2020-11-11 17:08:01 -0500

    added api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {useFindAndModify: false}).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).sort({day: -1}).limit(7).then(dbWorkout => {
+        res.json(dbWorkout)
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
