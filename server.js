18d21253fea9901da3a8dc25a9faa016cb068018
Tea Flores-Kestner <dfkestner@gmail.com>
2020-11-11 18:04:46 -0500

    edited route paths in server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
